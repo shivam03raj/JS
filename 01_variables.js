ac15348a213bca203efcb2d5ad753aafbe135c5b
Shivam <shivamraj3249@gmail.com>
2024-04-03 19:00:30 +0530

    repo

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const roll = 554
+let mail = "tyui@email"
+var marks = "1245"
+phone = "Bengal"
+console.table([roll, mail, marks, phone])
+
+mail = "iuioo"
+marks = "0860"
+phone = " nolia"
+let acc 
+
+console.table([roll, mail, marks, phone, acc])

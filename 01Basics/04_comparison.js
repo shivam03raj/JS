86e86c562fbbd6476ce4d163909db0db07ac106d
Shivam <shivamraj3249@gmail.com>
2024-05-25 10:58:31 +0530

    comparison revision

--- 01Basics/04_comparison.js
+++ 01Basics/04_comparison.js
@@ -1,16 +1,17 @@
 // console.log("2" > 1)
 // console.log("02" > 1)
 
-// console.log(null > 0)
+// console.log(null > 0) Here, null is treated as 0
 // console.log(null == 0)
-// console.log(null >= 0)
+// console.log(null >= 0) Here, null is trated as 0
 
 // console.log(undefined == 0)
 // console.log(undefined > 0)
 // console.log(undefined < 0)
 
+// NOTE: Comparison and equality check works different. In comparison, null becomes zero.
 
-// ===
+// === This is strict check, this not only check the value but the data type as well.
 
 
-console.log("2" === 2)
+console.log("2" === 2);

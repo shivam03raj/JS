ca3948f9e037c62b6a808b4fc8468f6339b8c24c
Shivam <shivamraj3249@gmail.com>
2024-05-26 12:35:41 +0530

    Dates revision

--- 01Basics/08_dates.js
+++ 01Basics/08_dates.js
@@ -1,5 +1,7 @@
 //Dates
 
+// Date is an object
+
 let date = new Date()
 console.log(date.toString());
 console.log(date.toDateString())
@@ -8,7 +10,7 @@ console.log(typeof(date))
 
 
 let myDate = new Date(2024, 0, 24)
-let createDate = new Date(2024, 0, 24, 9,23, 56)
+let createDate = new Date(2024, 0, 24, 9,23, 56) // Month begins from zero
 let createdDate = new Date("01-14-2024")
 console.log(createdDate.toLocaleString());
 

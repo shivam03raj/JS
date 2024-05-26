5cf84854162cba6bb2b0b3df03967911b62fba23
Shivam <shivamraj3249@gmail.com>
2024-05-26 09:52:31 +0530

    Maths and Num rrevision

--- 01Basics/07_nums_maths.js
+++ 01Basics/07_nums_maths.js
@@ -4,27 +4,27 @@ console.log(score);
 const balance = new Number(100)
 console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+console.log(balance.toString().length); // This changes the datatype of number to string and gives the length of that string. 
+console.log(balance.toFixed(1)); // toFixed is used to fix the number of digits after decimal point
 
 const otherNumber = 123.4932
 
-console.log(otherNumber.toPrecision(4));
+console.log(otherNumber.toPrecision(4)); // Gives the precise value for upto 3 digits.
 
 const num = 1000000
-console.log(num.toLocaleString('en-IN'));
+console.log(num.toLocaleString('en-IN')); // Gives the number a number system. It is US number system by default but we can mention the desired number system as well.
 
 //-------------------Maths---------------------------
 
-console.log(Math)
-console.log(Math.abs(-4));
+console.log(Math) // Math is an object
+console.log(Math.abs(-4)); // Abs means absolute it changes any negative number to positive.
 console.log(Math.round(4.6));
-console.log(Math.ceil(4.6));
-console.log(Math.floor(4.6));
+console.log(Math.ceil(4.6)); // It rounds the number to the top
+console.log(Math.floor(4.6)); // It rounds the number to the botttom
 console.log(Math.min(2,5,4,98,7,1));
-console.log(Math.min(2,5,4,98,7,1));
+console.log(Math.max(2,5,4,98,7,1));
 
-console.log(Math.random());
+console.log(Math.random()); // Gives a random value between 0 and 1 each time.  
 console.log(Math.random()*10);
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);

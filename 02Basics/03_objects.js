fca3ed18a40e8e69c958a0d7aabc2fc3fc6fe1f4
shivamraj3 <044shiv.am@gmail.com>
2024-05-31 09:11:32 +0530

    OBJECT REVISION

--- 02Basics/03_objects.js
+++ 02Basics/03_objects.js
@@ -5,6 +5,8 @@
 
 const mySym = Symbol("key1")
 
+const User = {} // This is an object. Now, we can add multiple keys and values as required. Keys can be written simply like 'name' or we can also write it as strings "name".
+
 // Behind the scenes, the keys are taken as strings.
 
 const JsUser= {
@@ -19,12 +21,14 @@ const JsUser= {
 }
 
 // console.log(JsUser.email) //One way of accessing the values
-// console.log(JsUser["email"]) 
-// console.log(JsUser["full name"])
+// console.log(JsUser[email]) // In this console, we will get error because behind the scene the keys are being tracked as strings. The below console for email is correct
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"]) // If we mentioned keys as strings, i.e. within double quotes then these cannot be accessed until we console it in the given manner.
 // console.log(JsUser[mySym])
 
-JsUser.email = "raj.grd@gmail.com"
+JsUser.email = "raj.grd@gmail.com" // To change any values
 // Object.freeze(JsUser) //After this point, the value of JsUser cannot be changed.
+
 JsUser.email = "raj@gmail.com"
 
 // console.log(JsUser)

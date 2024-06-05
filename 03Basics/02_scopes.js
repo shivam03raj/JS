03100e33f09171fc053825f5d00170226f59f9b4
shivamraj3 <044shiv.am@gmail.com>
2024-06-05 10:20:11 +0530

    Scope and hoisting

--- 03Basics/02_scopes.js
+++ 03Basics/02_scopes.js
@@ -17,10 +17,54 @@ let a = 300
 if(true){   // NOTE: Scope in console and scope in node is different 
     let a = 10
     const b = 20
-    console.log("INNER: ",a );
+    // console.log("INNER: ",a );
 }
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one(){
+    const username = "raj"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Raj"
+    if (username === "Raj"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ------------------------------Interesting--------------------------
+// These are two methods to make functions
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5) // This method of making functions will give error if we place the before the function declaration due to hoisting error, becuse we have held the function in a variable
+
+const addTwo = function(num){
+    return num + 2
+}
+
 
